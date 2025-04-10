a0a17d38bf0a57e730ecb48e2aaca4b1274b14dd
ojinikim <ojinikim@gmail.com>
2025-04-10 16:38:59 +0900

    feat :: 폰트 디자인 시스템 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,67 @@ export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        pretendard: ["Pretendard"],
+      },
+      fontSize: {
+        medium12: [
+          "12px",
+          {
+            fontWeight: 500,
+            lineHeight: "16px",
+          },
+        ],
+        medium14: [
+          "14px",
+          {
+            fontWeight: 400,
+            lineHeight: "20px",
+          },
+        ],
+        medium16: [
+          "16px",
+          {
+            fontWeight: 500,
+            lineHeight: "24px",
+          },
+        ],
+        medium18: [
+          "18px",
+          {
+            fontWeight: 500,
+            lineHeight: "24px",
+          },
+        ],
+        medium20: [
+          "20px",
+          {
+            fontWeight: 500,
+            lineHeight: "28px",
+          },
+        ],
+        semibold20: [
+          "20px",
+          {
+            fontWeight: 600,
+            lineHeight: "28px",
+          },
+        ],
+        semibold24: [
+          "24px",
+          {
+            fontWeight: 600,
+            lineHeight: "32px",
+          },
+        ],
+        semibold32: [
+          "32px",
+          {
+            fontWeight: 600,
+            lineHeight: "40px",
+          },
+        ],
+      },
       colors: {
         transparent: "transparent",
         white: "#FFFFFF",
@@ -28,6 +89,16 @@ export default {
         yellow700: "#A3B43F",
         yellow800: "#7F8C31",
         yellow900: "#616B25",
+        red50: "#FEF2F2",
+        red100: "#FEE2E2",
+        red200: "#FECACA",
+        red300: "#FCA5A5",
+        red400: "#F87171",
+        red500: "#EF4444",
+        red600: "#DC2626",
+        red700: "#B91C1C",
+        red800: "#991B1B",
+        red900: "#7F1D1D",
       },
     },
   },
