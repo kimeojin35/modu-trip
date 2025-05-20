f8896a1afee66cbb0c1e58e6c3564521f5443a38
ojinikim <ojinikim@gmail.com>
2025-05-20 13:38:06 +0900

    chore :: pr 리뷰 반영

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { Hide, Show } from '../assets';
 
 interface InputProps {
@@ -7,19 +7,18 @@ interface InputProps {
 	placeholder?: string;
 	description?: string;
 	value?: string;
-	onChange?: () => string;
+	onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
 	type?: 'password' | 'text';
 }
 export const Input = ({ title, placeholder, description, value, onChange, type }: InputProps) => {
 	const [showPassword, setShowPassword] = useState<boolean>(false);
-	console.log(showPassword ? 'show' : 'hide');
 
 	return (
 		<div className="flex flex-col w-full gap-2">
 			<p className="text-medium14 text-zinc-400">{title}</p>
 			<div className="flex hover:border-zinc-700 focus-within:hover:border-optic-600 focus-within:border-optic-600 transition-all w-full border rounded-md border-zinc-800 bg-zinc-900">
 				<input
-					type={showPassword && type == 'password' ? 'password' : 'text'}
+					type={!showPassword && type == 'password' ? 'password' : 'text'}
 					value={value}
 					onChange={onChange}
 					placeholder={placeholder}
