f1a3d91cdee5457940ac4e95693df313aa838949
ojinikim <ojinikim@gmail.com>
2025-05-20 12:16:53 +0900

    chore :: 아이콘명 변경

--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -1,28 +1,28 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
-import { Arrow } from "../assets";
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Arrow_Long } from '../assets';
 
 interface TopbarProps {
-  icon?: React.ReactNode;
-  onClick?: () => void;
-  title?: string;
+	icon?: React.ReactNode;
+	onClick?: () => void;
+	title?: string;
 }
 
 export const Topbar = ({ icon, onClick, title }: TopbarProps) => {
-  const navigate = useNavigate();
-  return (
-    <div className="fixed top-0 left-0 flex items-center justify-between w-full px-2 bg-[#111]">
-      <button onClick={() => navigate(-1)} className="p-4">
-        <Arrow direction="left" className="text-white" />
-      </button>
-      <p className="text-white text-medium16">{title}</p>
-      {icon ? (
-        <button onClick={onClick} className="p-4">
-          {icon}
-        </button>
-      ) : (
-        <div className="w-14 h-14" />
-      )}
-    </div>
-  );
+	const navigate = useNavigate();
+	return (
+		<div className="fixed top-0 left-0 flex items-center justify-between w-full px-2 bg-[#111]">
+			<button onClick={() => navigate(-1)} className="p-4">
+				<Arrow_Long direction="left" className="text-white" />
+			</button>
+			<p className="text-white text-medium16">{title}</p>
+			{icon ? (
+				<button onClick={onClick} className="p-4">
+					{icon}
+				</button>
+			) : (
+				<div className="w-14 h-14" />
+			)}
+		</div>
+	);
 };
